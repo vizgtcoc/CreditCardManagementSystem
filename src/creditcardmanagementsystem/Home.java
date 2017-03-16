c512c4d9264218abb8654bd8562478ae282bb742
Vishnu Maheswaran <vishnumaheswaran996@gmail.com>
2017-03-16 21:18:36 +0530

    it works

--- src/creditcardmanagementsystem/Home.java
+++ src/creditcardmanagementsystem/Home.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Git hub is working on this project
+
  */
 package creditcardmanagementsystem;
 import java.sql.*;
